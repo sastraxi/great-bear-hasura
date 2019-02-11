5acd41e854bfc71006e79439ed379265460c6eee
Cameron Gorrie <sastraxi@gmail.com>
2019-02-10 19:30:00 -0500

    start fleshing out a test client

---
+++ src/client.ts
@@ -0,0 +1,10 @@
+/*
+  TODO:
+    - (optionally) register a new user
+    - get logged in
+    - add some items to your cart (need to create a cart on first item addition)
+      - setCartQuantity(item_id, quantity)
+      - quantity can be zero to delete
+    - create the order, subscribe to the create_order row
+    - when we get the order id, subscribe to the order row, diff out updates to console
+*/
